65178ff362bd44540f1d4e8bb32be45c354fac8f
Dušan Suđić <duci.sudjic@gmail.com>
2024-11-10 10:35:03 +0100

    feat: acquirer and issuer run configurations

---
+++ Bank/back-end/Bank/src/main/resources/data-acquirer.sql
@@ -0,0 +1,4 @@
+INSERT INTO accounts (id, pan, expiration_date, card_holder_name, balance)
+VALUES ('1', '1234567890123456', '12/25', 'John Doe Acquirer', 1000.0),
+       ('2', '2345678901234567', '11/26', 'Jane Smith Acquirer', 1500.0),
+       ('3', '3456789012345678', '10/24', 'Alice Brown Acquirer', 1200.0);
--- Bank/back-end/Bank/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO accounts (id, pan, expiration_date, card_holder_name, balance)
-VALUES ('1', '1234567890123456', '12/25', 'John Doe', 1000.0),
-       ('2', '2345678901234567', '11/26', 'Jane Smith', 1500.0),
-       ('3', '3456789012345678', '10/24', 'Alice Brown', 1200.0);
