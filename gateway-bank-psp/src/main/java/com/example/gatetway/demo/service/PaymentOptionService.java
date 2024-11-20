f45e220dd005f4f6a66a6a72321f976cf54e422d
Milica Vujić <milicavujic2001@gmail.com>
2024-11-20 08:48:04 +0100

    feat: Added db in gateway for dynamic routing.

---
+++ gateway-bank-psp/src/main/java/com/example/gatetway/demo/service/PaymentOptionService.java
@@ -0,0 +1,22 @@
+package com.example.gatetway.demo.service;
+
+import com.example.gatetway.demo.model.PaymentOption;
+import com.example.gatetway.demo.repo.IPaymentOptionRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.PayloadApplicationEvent;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentOptionService {
+    @Autowired
+    IPaymentOptionRepo paymentOptionRepo;
+
+    public PaymentOptionService(IPaymentOptionRepo _paymentOptionRepo){
+        this.paymentOptionRepo=_paymentOptionRepo;
+    }
+
+    public String getAddressByOption(String option){
+        PaymentOption paymentOption=paymentOptionRepo.getPaymentOptionByOption(option);
+        return  paymentOption.getAddress();
+    }
+}
