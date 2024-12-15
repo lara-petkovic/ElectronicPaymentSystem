faf8453dda0489e98a556273dcd94d6c2d47dede
Milica Vujić <milicavujic2001@gmail.com>
2024-12-15 19:27:16 +0100

    fix: added controller to gw-psp-b.

---
+++ gateway-bank-psp/src/main/java/com/example/gatetway/demo/controller/GwRedirectController.java
@@ -0,0 +1,32 @@
+package com.example.gatetway.demo.controller;
+
+import com.example.gatetway.demo.model.NewTransactionDto;
+import com.example.gatetway.demo.service.PaymentOptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+@RequestMapping("/api/payments")
+public class GwRedirectController {
+
+    @Autowired
+    private PaymentOptionService paymentOptionService;
+
+    public GwRedirectController(PaymentOptionService paymentOptionService){
+        this.paymentOptionService=paymentOptionService;
+    }
+
+
+    @PostMapping
+    public void redirect (@RequestBody NewTransactionDto newtransaction,@RequestHeader("Payment") String option) throws Exception{
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:"+paymentOptionService.getAddressByOption(option).getPort()+paymentOptionService.getAddressByOption(option).getAddress();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(newtransaction.toString(), headers);
+        restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+    }
+}
---
+++ gateway-bank-psp/src/main/java/com/example/gatetway/demo/model/NewTransactionDto.java
@@ -0,0 +1,118 @@
+package com.example.gatetway.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class NewTransactionDto {
+    @JsonProperty("MerchantId")
+    private String MerchantId;
+
+    @JsonProperty("MerchantPassword")
+    private String MerchantPassword;
+
+    @JsonProperty("Amount")
+    private double Amount;
+
+    @JsonProperty("MerchantTimestamp")
+    private String MerchantTimestamp;
+
+    @JsonProperty("SuccessUrl")
+    private String SuccessUrl;
+
+    @JsonProperty("ErrorUrl")
+    private String ErrorUrl;
+
+    @JsonProperty("FailedUrl")
+    private String FailedUrl;
+
+    @JsonProperty("MerchantOrderId")
+    private String MerchantOrderId;
+
+    public NewTransactionDto() {}
+
+    public NewTransactionDto(String merchantId, String merchantPassword, double amount, String merchantTimestamp, String successUrl, String errorUrl, String failedUrl, String merchantOrderId) {
+        MerchantId = merchantId;
+        MerchantPassword = merchantPassword;
+        Amount = amount;
+        MerchantTimestamp = merchantTimestamp;
+        SuccessUrl = successUrl;
+        ErrorUrl = errorUrl;
+        FailedUrl = failedUrl;
+        MerchantOrderId = merchantOrderId;
+    }
+
+    public String getMerchantId() {
+        return MerchantId;
+    }
+
+    public void setMerchantId(String merchantId) {
+        MerchantId = merchantId;
+    }
+
+    public String getMerchantPassword() {
+        return MerchantPassword;
+    }
+
+    public void setMerchantPassword(String merchantPassword) {
+        MerchantPassword = merchantPassword;
+    }
+
+    public double getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(double amount) {
+        Amount = amount;
+    }
+
+    public String getMerchantTimestamp() {
+        return MerchantTimestamp;
+    }
+
+    public void setMerchantTimestamp(String merchantTimestamp) {
+        MerchantTimestamp = merchantTimestamp;
+    }
+
+    public String getSuccessUrl() {
+        return SuccessUrl;
+    }
+
+    public void setSuccessUrl(String successUrl) {
+        SuccessUrl = successUrl;
+    }
+
+    public String getErrorUrl() {
+        return ErrorUrl;
+    }
+
+    public void setErrorUrl(String errorUrl) {
+        ErrorUrl = errorUrl;
+    }
+
+    public String getFailedUrl() {
+        return FailedUrl;
+    }
+
+    public void setFailedUrl(String failedUrl) {
+        FailedUrl = failedUrl;
+    }
+
+    public String getMerchantOrderId() {
+        return MerchantOrderId;
+    }
+
+    public void setMerchantOrderId(String merchantOrderId) {
+        MerchantOrderId = merchantOrderId;
+    }
+
+    @Override
+    public String toString() {
+        return "{ \"MerchantId\" : \"" + MerchantId + "\", " +
+                "\"MerchantPassword\" : \"" + MerchantPassword + "\", " +
+                "\"Amount\" : \"" + Amount + "\", " +
+                "\"MerchantOrderId\" : \"" + MerchantOrderId + "\", " +
+                "\"MerchantTimestamp\" : \"" + MerchantTimestamp + "\", " +
+                "\"SuccessUrl\" : \""+SuccessUrl+"\", " +
+                "\"FailedUrl\" : \""+FailedUrl+"\", " +
+                "\"ErrorUrl\" : \""+ErrorUrl+"\" }";
+    }
+}
