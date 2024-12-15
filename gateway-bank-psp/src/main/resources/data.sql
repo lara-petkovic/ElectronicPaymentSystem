2930bf0313234724b70459d2b369d62a1d688e19
Milica Vujić <milicavujic2001@gmail.com>
2024-12-15 14:45:19 +0100

    feat: Added data.sql script for gateway-psp-bank.

---
+++ gateway-bank-psp/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO payment_option(
+    id, address, option, port)
+VALUES (1, '/api/transaction', 'Crypto', '8088');
+INSERT INTO payment_option(
+    id, address, option, port)
+VALUES (2, '/api/payments', 'Cart', '8052');
