1b8da3f9bb210c536e0744eb9e5af03db1472d1f
Lara <larapetkovic021@gmail.com>
2025-01-16 17:05:23 +0100

    feat: paypal https configuration

--- PayPal/paypal-back/src/main/java/com/sepproject/paypalback/PaypalBackApplication.java
+++ PayPal/paypal-back/src/main/java/com/sepproject/paypalback/PaypalBackApplication.java
@@ -1,13 +1,25 @@
 package com.sepproject.paypalback;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Objects;
+
 @SpringBootApplication
 public class PaypalBackApplication {
+    @Value("${MY_MASTER_PASSWORD}")
+    private  String password;
 
     public static void main(String[] args) {
         SpringApplication.run(PaypalBackApplication.class, args);
     }
 
+    @PostConstruct
+    public void init(){
+        String trustStorePath = Objects.requireNonNull(PaypalBackApplication.class.getClassLoader().getResource("keystore.jks")).getPath();
+        System.setProperty("javax.net.ssl.trustStore", trustStorePath);
+        System.setProperty("javax.net.ssl.trustStorePassword", password);
+    }
 }
