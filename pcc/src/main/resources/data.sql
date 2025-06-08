f898366fbb9acf78b67a3745abb11b0ff450876a
Dušan Suđić <duci.sudjic@gmail.com>
2025-06-08 10:51:46 +0200

    fix: paths for banks in pcc

--- pcc/src/main/resources/data.sql
+++ pcc/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO registered_bank(id,name,url)
-VALUES ('2345', 'Acquirer Bank', 'http://localhost:8052/api/payments/pay'),
-       ('6666', 'Issuer Bank', 'http://localhost:8051/api/payments/pay');
+VALUES ('2345', 'Acquirer Bank', 'https://localhost:8052/api/payments/pay'),
+       ('6666', 'Issuer Bank', 'https://localhost:8051/api/payments/pay');
